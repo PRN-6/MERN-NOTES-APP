f2ac25917308b6dba24930573e807299a495d7e5
PRN-6 <prinsonroyal2@gmail.com>
2025-11-26 20:13:24 +0530

    updated some stuf

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -25,13 +25,23 @@ export async function createNote(req , res){
 export async function updateNote(req , res){
     try{
         const {title,content} = req.body
-        await Note.findByIdAndUpdate(req.params.id,{title,content})
+        const updatedNote = await Note.findByIdAndUpdate(req.params.id,{title,content})
+        if(!updatedNote) return res.status(404).json({message:"Note not found"})
         res.status(200).json({message:"note updated  successfully"})
     }catch(error){
-        
+        console.error("error in updateNote controller",error)
+        res.status(500).json({message:"internal server error"})
     }
 }
 
 export async function deleteNote(req , res){
-    res.status(200).json({message: "note deleted"})
+    try{
+        const {title , content} = req.body
+        const deletedNote = await Note.findByIdAndDelete(req.params.id,{title,content})
+        if(!deletedNote) return res.status(404).json({message:"note not found"})
+        res.status(200).json({message:"note updated successfully"})
+    }catch(error){
+        console.error("error in deleteNote controller",error)
+        res.status(500).json({message:"internal server error"})
+    }
 }
