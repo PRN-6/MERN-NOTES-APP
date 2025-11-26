0a5fa233e415a516671b5975ed09ba380b32a0f1
PRN-6 <prinsonroyal2@gmail.com>
2025-11-26 20:10:46 +0530

    writen code for update and delete note

---
+++ src/controllers/notesController.js
@@ -0,0 +1,47 @@
+import Note from '../models/Note.js' 
+
+export  async function getAllNotes(req , res) {
+    try{
+        const notes = await Note.find()
+        res.status(200).json(notes)
+    }catch(error){
+        res.status(500).json({message: "Internal server error"})
+    }
+}
+
+export async function createNote(req , res){
+    try{
+        const {title,content} = req.body
+        const note = new Note({title , content})
+
+        const savednote = await note.save()
+        res.status(201).json(savednote)
+
+    }catch(error){
+        res.status(500).json({message: "Internal server error"})
+    }
+}
+
+export async function updateNote(req , res){
+    try{
+        const {title,content} = req.body
+        const updatedNote = await Note.findByIdAndUpdate(req.params.id,{title,content})
+        if(!updatedNote) return res.status(404).json({message:"Note not found"})
+        res.status(200).json({message:"note updated  successfully"})
+    }catch(error){
+        console.error("error in updateNote controller",error)
+        res.status(500).json({message:"internal server error"})
+    }
+}
+
+export async function deleteNote(req , res){
+    try{
+        const {title , content} = req.body
+        const deletedNote = await Note.findByIdAndDelete(req.params.id,{title,content})
+        if(!deletedNote) return res.status(404).json({message:"note not found"})
+        res.status(200).json({message:"note updated successfully"})
+    }catch(error){
+        console.error("error in deleteNote controller",error)
+        res.status(500).json({message:"internal server error"})
+    }
+}
