ffd530ab8640d4c25dca65dd32195a903158b0c4
prinsonnazareth2@gmail.com <prinsonnazareth2@gmail.com>
2025-11-28 22:49:18 +0530

    update project structure with new backend

---
+++ backend/src/models/Note.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+//create a schema
+//model based of that schema
+
+
+const noteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+
+},
+{timestamps: true}
+)
+
+
+const Note = mongoose.model("Note", noteSchema)
+
+export default Note
